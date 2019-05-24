16bd5721dce3eae6b5b1f6dba7f880cb3c72f3b6
Kaique Correia <kaiquecorreia@MacBook-Pro-de-Kaique.local>
2019-05-24 16:27:32 -0400

    feat: Readme

--- src/services/SocketListeners.js
+++
@@ -1,8 +0,0 @@
-const SocketListeners = (socket, io) => {
-  console.log(`Conectado ao socket ${socket.id}`)
-
-  socket.on('disconnect', function () {
-    console.log('User Disconnected')
-  })
-}
-module.exports = SocketListeners
